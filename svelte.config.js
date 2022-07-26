068846b89cdee1a880330030d148801ca96352fd
Joel K Rivers <48956906+aTurbulentMind@users.noreply.github.com>
2022-07-26 13:20:40 -0700

    Add files via upload

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocessor from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-netlify';
+import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocessor(),
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess(),
+
 	kit: {
 		adapter: adapter()
 	}
